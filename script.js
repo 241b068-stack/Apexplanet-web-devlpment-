f775da293e1b76988b490d994c78bd9a7145c66f
Ayush sharma <241b068@juetguna.in>
2026-06-30 20:41:57 +0530

    Delete script.js

--- script.js
+++
@@ -1,4 +0,0 @@
-// Simple alert prompt validation interaction
-document.getElementById('alertBtn').addEventListener('click', () => {
-    alert('Task 1 complete! Basics of JavaScript Interactivity are functional.');
-});
